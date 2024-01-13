ffcfb389d04455ba89e5d91332acf7b1b421276c
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-13 16:28:36 +0100

    ‒ Działanie kontrolek obsłużone za pomocą zdarzeń lub techniką Data Binding  ✅😎

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,15 +1,60 @@
-﻿namespace Kotek
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Kotek
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
         private string[] images = { "kot1.jpg", "kot2.jpg", "kot3.jpg", "kot4.jpg" };
         private int currentIndex = 0;
+        private bool isBlueTheme = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            UpdateImageSource();
         }
 
+        public string CurrentImagePath => images[currentIndex];
+        public string CurrentImageNumber { get; set; }
+        public bool IsBlueTheme
+        {
+            get => isBlueTheme;
+            set
+            {
+                if (isBlueTheme != value)
+                {
+                    isBlueTheme = value;
+                    OnPropertyChanged();
+                    UpdateTheme();
+                }
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateImageSource()
+        {
+            OnPropertyChanged(nameof(CurrentImagePath));
+        }
+
+        private void UpdateTheme()
+        {
+            if (IsBlueTheme)
+            {
+                this.BackgroundColor = Color.FromRgb(0, 0, 255);
+            }
+            else
+            {
+                this.BackgroundColor = Color.FromRgb(0, 128, 0); // Zielony kolor
+            }
+        }
         private void PrevImage(object sender, EventArgs e)
         {
             currentIndex--;
@@ -28,12 +73,6 @@
                 currentIndex = 0;
             }
             UpdateImageSource();
-        }
-
-        private void UpdateImageSource()
-        {
-            string imagePath = images[currentIndex];
-            image.Source = imagePath;
         }
 
         private void UpdateImageFromText()
